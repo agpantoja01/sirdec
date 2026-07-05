fd49703cdab738e99813b7e15e8ba934193fe747
agpantoja01 <agpantoja01@gmail.com>
2026-07-04 20:11:29 -0500

    Update innerHTML and add casoActivo text content

--- script.js
+++ script.js
@@ -202,6 +202,9 @@ window.abrirCaso = async function () {
 
             div.innerHTML =
                 datos[i] || "";
+   
+    document.getElementById("casoActivo").textContent =
+    "Caso: " + numeroCaso;
 
         });
 
