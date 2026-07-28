9840305f6880cbc60a40aa2245b9bfec82856dbd
agpantoja01 <agpantoja01@gmail.com>
2026-07-28 14:11:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -380,3 +380,13 @@ window.irAHallazgos = function () {
         });
 
 };
+
+const botonMenu = document.getElementById("btnMenu");
+
+const menu = document.getElementById("menuLateral");
+
+botonMenu.addEventListener("click", () => {
+
+    menu.classList.toggle("menuOculto");
+
+});
